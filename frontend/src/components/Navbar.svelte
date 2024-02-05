e57e3a122936831d7477d17b0303054615ad2509
Hugo Olsson <hugo.contact01@gmail.com>
2024-02-05 10:58:05 +0100

    Added resizing logic and updated icon.

--- frontend/src/components/Navbar.svelte
+++ frontend/src/components/Navbar.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <div class="fixed z-20 flex items-center p-2 right-0 left-0 {STD.paddingStyle} bg-white/70 backdrop-blur-md">
-  <img src={"/logo.svg"} alt="logo svg" class="w-8 h-8" />
+  <a href="/"><img src={"/logo.svg"} alt="logo svg" class="w-8 h-8" /></a>
   <Spacer />
   <button>
     <img src={"/navIcon.svg"} alt="Navigation menu icon" class="w-5 h-5" />
--- frontend/src/components/building-page/RoomCell.svelte
+++ frontend/src/components/building-page/RoomCell.svelte
@@ -5,16 +5,18 @@
   let isAvailable = Math.random() < 0.5
 </script>
 
-<div class="bg-dark/10 w-full flex items-center gap-2 p-2 rounded-md">
-  <div class="flex flex-col">
-    <p class="font-bold">EG-{Math.round(Math.random() * 6000)}</p>
+<div class="bg-dark/10 w-full flex flex-col tablet:flex-row items-center p-2 rounded-md">
+  <div class="flex flex-col self-start tablet:self-center">
+    <p class="font-bold">EG-{1000 + Math.round(Math.random() * 6000)}</p>
     <p class="font-light text-smallContent">This is a description that tells the user about the room.</p>
   </div>
-
   <Spacer />
-  <p class="{isAvailable ? ' text-' : ''} font-bold capitalize text-smallContent">
-    {isAvailable ? "Available" : "Used"}
-  </p>
-  <IconButton iconName={"campusmaps.svg"} />
-  <IconButton iconName={"timeedit.svg"} />
+  <div class="flex gap-1 items-center self-end tablet:self-center">
+    <Spacer />
+    <p class="{isAvailable ? ' text-[#009760]' : 'text-[#97005A]'} font-bold text-smallContent">
+      {isAvailable ? "AVAILABLE" : "USED"}
+    </p>
+    <IconButton iconName={"campusmaps.svg"} />
+    <IconButton iconName={"timeedit.svg"} />
+  </div>
 </div>
