d28f6e69ba3d60ecb91c03365a05ea24448d86b2
Hugo Olsson <hugo.contact01@gmail.com>
2024-02-05 11:05:32 +0100

    Vercel update.

--- frontend/src/components/building-page/RoomCell.svelte
+++ frontend/src/components/building-page/RoomCell.svelte
@@ -7,7 +7,7 @@
 
 <div class="bg-dark/10 w-full flex flex-col tablet:flex-row items-center p-2 rounded-md">
   <div class="flex flex-col self-start tablet:self-center">
-    <p class="font-bold">EG-{1000 + Math.round(Math.random() * 6000)}</p>
+    <p class="font-bold">EG-{1000 + Math.round(Math.random() * 5000)}</p>
     <p class="font-light text-smallContent">This is a description that tells the user about the room.</p>
   </div>
   <Spacer />
