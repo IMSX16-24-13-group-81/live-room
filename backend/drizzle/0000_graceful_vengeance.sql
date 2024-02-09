a90b2e1e5fc1a61445c92376f2793416eab3d199
Hugo Olsson <hugo.contact01@gmail.com>
2024-02-09 11:20:14 +0100

    Setup with node, typescript, fastify, drizzle.

---
+++ backend/drizzle/0000_graceful_vengeance.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS "buildings" (
+	"id" serial NOT NULL,
+	"name" text,
+	"description" text
+);
---
+++ backend/src/db/schema.ts
@@ -0,0 +1,7 @@
+import { serial, text, pgTable } from "drizzle-orm/pg-core"
+
+export const building = pgTable("buildings", {
+  id: serial("id"),
+  name: text("name"),
+  description: text("description"),
+})
