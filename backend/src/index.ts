97a7ff88549f26aa19afb7aaa09f969450b33c71
Oscar Eriksson <oscariremma@gmail.com>
2024-02-16 10:02:19 +0100

    Add docker files for backend and frontend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,15 @@
-import fastify from "fastify"
+import fastify from "fastify";
 
-const server = fastify()
+const server = fastify();
 
 server.get("/ping", async (request, reply) => {
-  return "pong\n"
-})
+  return "pong\n";
+});
 
-server.listen({ port: 8080 }, (err, address) => {
+server.listen({ port: 8080, host: "0.0.0.0" }, (err, address) => {
   if (err) {
-    console.error(err)
-    process.exit(1)
+    console.error(err);
+    process.exit(1);
   }
-  console.log(`Server listening at ${address}`)
-})
+  console.log(`Server listening at ${address}`);
+});
