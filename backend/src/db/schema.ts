f0437c8f96ae603c87217dd7f0d7c2d3b91bec9d
Hugo Olsson <hugo.contact01@gmail.com>
2024-02-28 00:30:30 +0100

    Batched with previous commit.

--- backend/src/db/schema.ts
+++ backend/src/db/schema.ts
@@ -1,7 +1,14 @@
-import { serial, text, pgTable } from 'drizzle-orm/pg-core';
+import { serial, text, boolean, pgTable } from 'drizzle-orm/pg-core';
 
-export const building = pgTable('buildings', {
-  id: serial('id'),
+export const buildings = pgTable('buildings', {
+  id: serial('id').primaryKey(),
   name: text('name'),
   description: text('description')
+});
+
+export const rooms = pgTable('rooms', {
+  id: serial('id').primaryKey(),
+  name: text('name'),
+  coordiates: text('coordiates'),
+  available: boolean('available')
 });
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,39 +1,46 @@
 import fastify from 'fastify';
 import { heartbeat, updateOccupants } from './influx/sensors';
+import { getDB } from './db/config';
 
-const server = fastify();
+//For async runtime without top-level-async-tricks.
+const main = async () => {
+  const server = fastify();
+  const db = await getDB();
 
-server.get('/api/ping', async (request, reply) => {
-  return 'pong\n';
-});
+  server.get('/api/ping', async (request, reply) => {
+    return 'pong\n';
+  });
 
-server.post('/api/sensors/report', async (request, reply) => {
-  const { occupants, sensorId, authorization }: any = request.body;
-  updateOccupants(occupants, sensorId);
-  return 'Success';
-});
+  server.post('/api/sensors/report', async (request, reply) => {
+    const { occupants, sensorId, authorization }: any = request.body;
+    updateOccupants(occupants, sensorId);
+    return 'Success';
+  });
 
-server.get('/api/sensors/report/test', async (request, reply) => {
-  const randomOccupants = Math.floor(Math.random() * 10);
-  updateOccupants(randomOccupants, 'sensor1');
-  return 'Success';
-});
+  server.get('/api/sensors/report/test', async (request, reply) => {
+    const randomOccupants = Math.floor(Math.random() * 10);
+    updateOccupants(randomOccupants, 'sensor1');
+    return 'Success';
+  });
 
-server.post('/api/sensors/heartbeat', async (request, reply) => {
-  const { sensorId, firmwareVersion, authorization }: any = request.body;
-  heartbeat(sensorId, firmwareVersion);
-  return 'Success';
-});
+  server.post('/api/sensors/heartbeat', async (request, reply) => {
+    const { sensorId, firmwareVersion, authorization }: any = request.body;
+    heartbeat(sensorId, firmwareVersion);
+    return 'Success';
+  });
 
-server.get('/api/sensors/heartbeat/test', async (request, reply) => {
-  heartbeat('sensor1', '0.0.1');
-  return 'Success';
-});
+  server.get('/api/sensors/heartbeat/test', async (request, reply) => {
+    heartbeat('sensor1', '0.0.1');
+    return 'Success';
+  });
 
-server.listen({ port: 8080, host: '0.0.0.0' }, (err, address) => {
-  if (err) {
-    console.error(err);
-    process.exit(1);
-  }
-  console.log(`Server listening at ${address}`);
-});
+  server.listen({ port: 8080, host: '0.0.0.0' }, (err, address) => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    console.log(`Server listening at ${address}`);
+  });
+};
+
+main();
