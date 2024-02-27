36dcb4e8a432566823af48374e12606695a07d18
GAsplund <gasplund2@gmail.com>
2024-02-27 10:56:35 +0100

    Use /api/

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,29 +3,29 @@ import { heartbeat, updateOccupants } from "./influx/sensors";
 
 const server = fastify();
 
-server.get("/ping", async (request, reply) => {
+server.get("/api/ping", async (request, reply) => {
   return "pong\n";
 });
 
-server.post("/sensors/report", async (request, reply) => {
+server.post("/api/sensors/report", async (request, reply) => {
   const { occupants, sensorId, authorization }: any = request.body;
   updateOccupants(occupants, sensorId);
   return "Success";
 });
 
-server.get("/sensors/report/test", async (request, reply) => {
+server.get("/api/sensors/report/test", async (request, reply) => {
   const randomOccupants = Math.floor(Math.random() * 10);
   updateOccupants(randomOccupants, "sensor1");
   return "Success";
 });
 
-server.post("/sensors/heartbeat", async (request, reply) => {
+server.post("/api/sensors/heartbeat", async (request, reply) => {
   const { sensorId, firmwareVersion, authorization }: any = request.body;
   heartbeat(sensorId, firmwareVersion);
   return "Success";
 });
 
-server.get("/sensors/heartbeat/test", async (request, reply) => {
+server.get("/api/sensors/heartbeat/test", async (request, reply) => {
   heartbeat("sensor1", "0.0.1");
   return "Success";
 });
