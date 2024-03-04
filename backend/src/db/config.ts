01ba465cc496bbc796cdcdf461bf7359cdacad5a
Goostaf <gasplund2@gmail.com>
2024-03-04 14:24:51 +0100

    Use database connection environment variable

--- backend/src/db/config.ts
+++ backend/src/db/config.ts
@@ -4,13 +4,15 @@ import { migrate } from 'drizzle-orm/node-postgres/migrator';
 import * as schema from './schema';
 
 export const getPG = async () => {
-  const client = new Client({
-    host: '127.0.0.1',
-    port: 5432,
-    user: 'liveinfo',
-    password: 'livepassword',
-    database: 'liveinfo'
-  });
+  const client = new Client(
+    process.env.PG_CONNECTION_STRING || {
+      host: '127.0.0.1',
+      port: 5432,
+      user: 'liveinfo',
+      password: 'livepassword',
+      database: 'liveinfo'
+    }
+  );
 
   await client.connect();
   const db = drizzle(client, { schema });
