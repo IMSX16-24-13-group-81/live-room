5967d1dd8fec092778d1066ab26b2be1d7b15743
Hugo Olsson <hugo.contact01@gmail.com>
2024-04-09 11:50:35 +0200

    Feedback and polling. Polling harder to test locally since no data.

--- frontend/src/lib/STD.ts
+++ frontend/src/lib/STD.ts
@@ -1,6 +1,7 @@
 const STD = {
   paddingStyle:
     "pr-[4%] pl-[4%] tablet:pr-[4%] tablet:pl-[4%] laptop:pr-[8%] laptop:pl-[8%] desktop:pl-[20%] desktop:pr-[20%] bigScreen:pl-[27%] bigScreen:pr-[27%]",
+  pollingInterval: 30 * 1000, // Half a minute
 }
 
 export default STD
