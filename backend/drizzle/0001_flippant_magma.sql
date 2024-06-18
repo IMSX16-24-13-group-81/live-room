bd673942399dd3219886ccd743db04dc269a3462
Isak Nordin <isaknor@student.chalmers.se>
2024-06-18 09:49:30 +0200

    En rolig branch, roomId kan komma att raderas

--- backend/drizzle/0001_flippant_magma.sql
+++ backend/drizzle/0001_flippant_magma.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS "sensors" (
 	"id" text PRIMARY KEY NOT NULL,
-	"room" serial NOT NULL
+	"room" text NOT NULL
 );
 --> statement-breakpoint
 DO $$ BEGIN
- ALTER TABLE "sensors" ADD CONSTRAINT "sensors_room_rooms_id_fk" FOREIGN KEY ("room") REFERENCES "rooms"("id") ON DELETE no action ON UPDATE no action;
+ ALTER TABLE "sensors" ADD CONSTRAINT "sensors_room_rooms_name_fk" FOREIGN KEY ("room") REFERENCES "rooms"("name") ON DELETE no action ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
