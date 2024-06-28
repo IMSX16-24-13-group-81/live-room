e22e322e21734ab52afd6a04bfcd5b9641fbefe7
Isak Nordin <isaknor@student.chalmers.se>
2024-06-28 14:41:27 +0200

    yes

--- frontend/src/routes/stats/+page.svelte
+++ frontend/src/routes/stats/+page.svelte
@@ -59,9 +59,6 @@
   <Spacer height={40} />
   <CustomButton text={"SEE DATA"} disabled={tokenString === ""} on:click={() => testAccess()} />
 
-  <Spacer height={40} />
-  <CustomButton text={"ADD ROOM"} disabled={tokenString === ""} on:click={() => handlesu()} />
-
   <Spacer height={10} />
 
   {#if showIncorrectToken}
