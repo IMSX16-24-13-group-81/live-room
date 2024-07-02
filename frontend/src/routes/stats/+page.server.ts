08bf39f768bc566226c186456ec5703eb2928a96
Isak Nordin <isaknord@outlook.com>
2024-07-02 16:50:22 +0200

    start on addRooms for frontend

--- frontend/src/routes/stats/+page.server.ts
+++ frontend/src/routes/stats/+page.server.ts
@@ -1,10 +1,11 @@
-import { getAllRooms } from "$lib/helpers"
+import { getAllRooms, addRooms } from "$lib/helpers"
 import type { PageServerLoad } from "./$types"
 
 export const load = (async ({ params }) => {
   const allRooms = await getAllRooms()
+  const add = await addRooms()
 
   return {
-    allRooms,
+    allRooms, add
   }
 }) satisfies PageServerLoad
