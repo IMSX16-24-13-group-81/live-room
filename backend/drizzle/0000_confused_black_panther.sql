571066efbc0368d45ff7188c50a6840c2e878dad
Ella Dahlström <elladah@student.chalmers.se>
2024-08-01 13:31:04 +0200

    addRoom update

--- backend/drizzle/0000_confused_black_panther.sql
+++ backend/drizzle/0000_confused_black_panther.sql
@@ -8,5 +8,8 @@ CREATE TABLE IF NOT EXISTS "rooms" (
 	"id" serial PRIMARY KEY NOT NULL,
 	"name" text,
 	"coordinates" text,
-	"available" boolean
+	"available" boolean,
+	"building" integer NOT NULL,
+    "description" text,
+    CONSTRAINT "rooms_building_fk" FOREIGN KEY ("building") REFERENCES "buildings"("id") ON DELETE NO ACTION ON UPDATE NO ACTION
 );
