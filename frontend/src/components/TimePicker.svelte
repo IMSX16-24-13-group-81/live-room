52dc6fdc6d7667599e917748e8f9e5fdf4161e49
Ella Dahlström <elladah@student.chalmers.se>
2024-08-07 09:59:37 +0200

    Historical statistics in frontend

---
+++ frontend/src/components/TimePicker.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    export let selectedTime: string | null = null;
+    export let label: string = "Select Time";
+  
+    const handleChange = (event: Event) => {
+      const target = event.target as HTMLInputElement;
+      selectedTime = target.value;
+    };
+  </script>
+  
+  <label>
+    {label}
+    <input type="time" on:change={handleChange} />
+  </label>
--- frontend/src/routes/stats/+page.server.ts
+++ frontend/src/routes/stats/+page.server.ts
@@ -7,3 +7,20 @@ export const load = (async ({ params }) => {
     allRooms
   }
 }) satisfies PageServerLoad
+
+//Call for statistics for certain dates and time
+export const actions = {
+  getRoomDataForRange: async ({ request }) => {
+    const { startDateTime, endDateTime } = await request.json();
+    
+    // Use startDateTime and endDateTime to query the InfluxDB
+    const lineChartData = await getRoomDataForRange(startDateTime, endDateTime);
+
+    return {
+      status: 200,
+      body: {
+        lineChartData
+      }
+    };
+  }
+};
